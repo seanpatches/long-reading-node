9a185cd0ce91a8eb16a63201a6d81d5d3baafdce
Sean Nelson <smnelsonn@gmail.com>
2019-04-14 14:55:47 -0700

    pg 37

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('hello world');
+const server = require('./server');
+
+server.start();
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const http = require('http');
+var http = require('http');
+const url = require('url');
 
-http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text-plain' }); 
-  response.write('Hello World');
-  response.end('');
-}).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
+
+    console.log('Request for ' + pathname + 'received.');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello World');
+    response.end();
+  }
+ 
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
+exports.start = start;
