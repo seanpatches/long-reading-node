1912c7d17c71b20dfcd8bb343f99e6aaa6d7befb
Sean Nelson <smnelsonn@gmail.com>
2019-04-14 15:11:09 -0700

    pg 44

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require('http');
 const url = require('url');
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
 
     console.log('Request for ' + pathname + 'received.');
+
+    route(pathname);
+    
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
     response.end();
